ddd89417c41079b8b2798bfb533f0acad01dd475
Cemes99 <55666366+Cemes99@users.noreply.github.com>
2022-03-30 07:10:13 +0700

    add feature for admin

---
+++ HomeAdmin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class HomeAdmin : Form
+    {
+        public HomeAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            new EmployeeManagement().Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            new ReportForm().Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            new Login().Show();
+        }
+    }
+}
